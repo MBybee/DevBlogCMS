c9217f73b13e0da3f3120a60a2693b5ad605fefb
M Bybee <675944+MBybee@users.noreply.github.com>
2020-05-24 15:31:17 -0700

    Updates to change locations
    Change where files are stored by default (project/files instead of author/project/files), some cleanup to remove some book-specific output text.

--- BookshelfCMS.pl
+++ BookshelfCMS.pl
@@ -13,53 +13,76 @@ use JSON;
 use Roman;
 
 my $usage = "BookshelfCMS requires at least the name of a file to format. \nPass directories or filenames to import \n--help for this message, or --version to get the current version";
-if ( $#ARGV < 0 ){
+if ( $#ARGV < 0 ) {
 	die "$usage";
 }
 
-my($help, $ver, $inpath);
+my ($help, $ver, $inpath);
 GetOptions(
 	"help|?"		=> \$help,
 	"version|about|V"	=> \$ver,
 );
-if( defined($ver)  ){ version(); exit; }
-if( defined($help) ){ print "$usage\n" ; exit; }
+if ( defined($ver)  ) { version(); exit; }
+if ( defined($help) ) { print "$usage\n" ; exit; }
 
 my %result = loadLibrary();
-if ( $result{'version'} eq "" ){ die( "Library issue: $!"); }
+if ( $result{'version'} eq "" ) { die( "Library issue: $!"); }
+
+
+my $SiteRoot	= 'https://yoursite.org/';
+my $Twitter	= '@yoursite';
+my $Site	= 'yoursite';
+my $Logo	= 'yourlogo.svg';
+my $SiteCopy	= '<figure><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img class="logo" alt="Creative Commons License" src="https://yoursite.org/images/cc-by-sa.svg" width="80"></a></figure><p><a property="cc:attributionName" href="https://yoursite.org">yoursite.org blogs</a> are licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.';
+my $StylePath	= 'css/';
+my $ImagePath	= 'images/';
+my $config_file = "config.txt";
+if ( -e $config_file ) { 
+	open my $fh, '<', $config_file or warn "Unable to open config file $config_file: $!\n Proceeding with defaults.";
+	while( my $fl = <$fh> ){
+		my @line = split /;;/, $fl;
+		if ( ($line[0] eq "SiteRoot") && ($line[1] ne "") ){ 		$SiteRoot 	= $line[1]; }
+		elsif ( ($line[0] eq "Twitter") && ($line[1] ne "") ){ 		$Twitter 	= $line[1]; }
+		elsif ( ($line[0] eq "Site") && ($line[1] ne "") ){ 		$Site 		= $line[1]; }
+		elsif ( ($line[0] eq "SiteCopy") && ($line[1] ne "") ){ 	$SiteCopy 	= $line[1]; }
+		elsif ( ($line[0] eq "StylePath") && ($line[1] ne "") ){ 	$StylePath 	= $line[1]; }
+		elsif ( ($line[0] eq "ImagePath") && ($line[1] ne "") ){ 	$ImagePath 	= $line[1]; }
+	}
+	close $fh;
+}
+
 
 # Set these values as required for the site
-setSiteroot('https://yoursite.org/');
-setTwitter('@yoursite');
-setSite('yoursite');
-setLogo('yourlogo.svg');
-setSitecopy('<figure><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img class="logo" alt="Creative Commons License" src="https://yoursite.org/images/cc-by-sa.svg" width="80"></a>
-</figure><p><a property="cc:attributionName" href="https://yoursite.org">yoursite.org blogs</a> are licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.');
-setStylepath('css/');
-setImagepath('images/');
+setSiteroot($SiteRoot);
+setTwitter($Twitter);
+setSite($Site);
+setLogo($Logo);
+setSitecopy($SiteCopy);
+setStylepath($StylePath);
+setImagepath($ImagePath);
 useIndex(1);
 
 # Get the config and prepare the filesystem to write out our chapter
-foreach my $dir ( @ARGV ){
+foreach my $dir ( @ARGV ) {
 	#print "Is $dir a directory or a file?\n";
-	if( -d $dir ){
+	if ( -d $dir ) {
 		opendir( INDIR, $dir ) or die "Cannot open $dir: $!\n";
 		my @files = readdir( INDIR );
 		my $file;
 		closedir( INDIR );
 		# Process the actual files first to get things like style/image/author paths etc
-		foreach $file ( sort(@files) ){
+		foreach $file ( sort(@files) ) {
 			my $path = File::Spec->catfile($dir, $file);
-			if( -d $path ){ next; }
+			if ( -d $path ) { next; }
 			elsif( $file =~ m/txt|html/ ){				
 				print "Importing $path\n";
-				if( !initChapter($path) ){ die "Unable to create chapter: $!"; }
+				if ( !initChapter($path) ) { die "Unable to create chapter: $!"; }
 				#writeChapter($path);
 			}
 		}
 		foreach $file ( @files ){
 			my $path = File::Spec->catfile($dir, $file);
-			if( -d $path ){ next; }
+			if ( -d $path ){ next; }
 			elsif( $file =~ m/txt|html/ ){				
 				print "Writing $path\n";
 				writeChapter($path);
@@ -74,21 +97,21 @@ foreach my $dir ( @ARGV ){
 			}
 		}
 	}
-	else{
-		foreach( sort(@ARGV) ){
+	else {
+		foreach ( sort(@ARGV) ) {
 			print "Processing $_\n";
-			if( !initChapter($_) ){ die "Unable to create chapter: $!"; }
+			if ( !initChapter($_) ){ die "Unable to create chapter: $!"; }
 		}
-		foreach( @ARGV ){
+		foreach ( @ARGV ) {
 			print "Writing $_\n";
 			writeChapter($_);
 		}
-		foreach ( @ARGV ){
+		foreach ( @ARGV ) {
 			# This will pass files and directories to moveFile if they are the right sort
 			my $path = File::Spec->catfile($dir, $_);
-			if( $_ =~ m/.jpg|.gif|.png|.svg|css|image|style/ ){
-				if( -d $path ){ moveDir($path); }
-				else{ moveFile($dir, $_); }
+			if ( $_ =~ m/.jpg|.gif|.png|.svg|css|image|style/ ) {
+				if ( -d $path ) { moveDir($path); }
+				else { moveFile($dir, $_); }
 			}
 		}
 	}
@@ -96,8 +119,8 @@ foreach my $dir ( @ARGV ){
 }
 
 	
-if( writeBook() ){ print "Site written\n"; }
-if( !writeAuthor() ){ die( "Unable to update the author site: $!"); }
-if( !updateLibrary() ){ die( "Unable to save changes to library: $!"); }
+if ( writeBook() ) { print "Site written\n"; }
+if ( !writeAuthor() ) { die( "Unable to update the author site: $!"); }
+if ( !updateLibrary() ) { die( "Unable to save changes to library: $!"); }
 
 
